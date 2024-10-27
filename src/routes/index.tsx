bbfe1e32d91c3e119a4948cdbddad8b4a2c50476
Daniel Macak <daniel.macak@email.cz>
2024-10-27 15:36:23 +0100

    Overflow repro

---
+++ src/routes/index.tsx
@@ -0,0 +1,172 @@
+import { Checkbox } from "~/components/ui/checkbox";
+
+export default function Home() {
+  return (
+    <main class="text-center mx-auto text-gray-700 p-4 bg-pink-100 h-full">
+      <div class="ml-auto bg-white rounded-lg overflow-auto max-h-[calc(100vh-2rem)] w-1/2">
+        <ul>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+          <li class="flex items-center gap-2 p-2 border-b border-neutral-200">
+            <Checkbox />
+            List item
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
